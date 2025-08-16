a321c6285cc234046e77ef1ecde803b69cfa552e
Fede Saenz <fedesaenz97@gmail.com>
2025-08-16 20:31:57 -0300

    Genero base de la estructura del microservicio geocodificador

---
+++ geocodificador/src/main/java/challenge/geocodificador/GeocodificadorApplication.java
@@ -0,0 +1,13 @@
+package challenge.geocodificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodificadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocodificadorApplication.class, args);
+	}
+
+}
---
+++ geocodificador/src/test/java/challenge/geocodificador/GeocodificadorApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.geocodificador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodificadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
